94518cf0ec6c4ffc8ac130d938b76477108a237d
drishtikedia <drishti.kedia@vaartani.com>
2020-10-11 17:29:30 +0530

    card and steps added

--- stepper/src/App.js
+++ stepper/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import HorizontalLabelPositionBelowStepper from './Step';
+
 
 
 function App() {
   return (
-    <div className="App">
-      app
+    <div >
+      <HorizontalLabelPositionBelowStepper/>
+      
+
     </div>
   );
 }
